a13c17dc8cc99aee2d16eea4de8a12bbaf01e00d
rpzbass <ronaldop.tech@gmail.com>
2021-07-29 11:50:57 -0300

    Teste com outra data

--- devBasicJavaDigitalOne/src/devBasicJavaDigitalOne/date/ExercicioFinal.java
+++ devBasicJavaDigitalOne/src/devBasicJavaDigitalOne/date/ExercicioFinal.java
@@ -15,7 +15,7 @@ public class ExercicioFinal {
 		 */
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Date aComparar = sdf.parse("15/05/2010");
-		Date aniversario = sdf.parse("08/11/1983");
+		Date aniversario = sdf.parse("06/10/1990");
 		System.out.println(aniversario.getTime());
 		System.out.println(aniversario.after(aComparar)?"É Depois":aniversario.before(aComparar)?"É realmente antes":"É depois");
 		
