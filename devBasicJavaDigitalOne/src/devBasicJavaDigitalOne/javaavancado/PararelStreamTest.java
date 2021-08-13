3422a0392949acc7eac65caffc23b39da4a85ac5
rpzbass <ronaldop.tech@gmail.com>
2021-08-13 19:42:16 -0300

    parallelStreams

--- devBasicJavaDigitalOne/src/devBasicJavaDigitalOne/javaavancado/PararelStreamTest.java
+++ devBasicJavaDigitalOne/src/devBasicJavaDigitalOne/javaavancado/PararelStreamTest.java
@@ -1,5 +1,7 @@
 package devBasicJavaDigitalOne.javaavancado;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class PararelStreamTest {
@@ -7,22 +9,21 @@ public class PararelStreamTest {
 	public static void main(String[] args) {
 
 		long inicio = System.currentTimeMillis();
-		IntStream.range(1, 100000).forEach(num ->  fatorial(num));
+		IntStream.range(1, 1000).forEach(num ->  fatorial(num));
 		long fim = System.currentTimeMillis();
 		System.out.println("Tempo de execução: " + (fim - inicio));
 
+		List<String> names = Arrays.asList("Joao","Paulo","Oliveira","Santos");
+		names.parallelStream().forEach(System.out::println);	
+	
 	}
-
+	
 	public static long fatorial(long num) {
-
 		long fat = 1;
-		
 		for (long i = 2; i <= num; i++) {
 			System.out.println("Executando:");
 			fat *= i;
 		}
-
 		return fat;
 	}
-
 }
